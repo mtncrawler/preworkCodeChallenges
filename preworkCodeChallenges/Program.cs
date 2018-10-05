f838ad6762901a97d2de766a322f95ace316353b
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-05 10:33:55 -0700

    Add project files.

---
+++ preworkCodeChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace preworkCodeChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
