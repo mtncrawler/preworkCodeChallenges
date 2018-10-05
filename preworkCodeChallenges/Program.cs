f43e79504713a7de4c2198ef0471081a8fc1f129
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-05 11:58:02 -0700

    completed first challenge

--- preworkCodeChallenges/Program.cs
+++ preworkCodeChallenges/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace preworkCodeChallenges
+namespace codeChallenges
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Array Max Result");
+            int[] randomArrayOutput = RandomArray();
+            Console.WriteLine("[{0}, {1}, {2}, {3}, {4}]", 
+                randomArrayOutput[0],
+                randomArrayOutput[1],
+                randomArrayOutput[2],
+                randomArrayOutput[3],
+                randomArrayOutput[4]);
+            Console.Write("Please select a number from the array: ");
+            string userValue = Console.ReadLine();
+            Console.WriteLine("Your score: " + ArrayMaxResult(randomArrayOutput, userValue));
+            Console.ReadLine();
         }
+
+        static int[] RandomArray()
+        {
+            int[] randomNum = new int[5];
+            Random nums = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                randomNum[i] = nums.Next(1, 10);
+            }
+            return randomNum;
+        }
+
+        static int ArrayMaxResult(int[] arrInts, string userValue)
+        {
+            int counter = 0;
+            int num = Int32.Parse(userValue);
+            foreach (int number in arrInts)
+            {
+                if (number == num)
+                {
+                    counter++;
+                }
+            }
+            return num * counter;
+        }
+
     }
 }
